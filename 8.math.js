538c6f291d0f33782c99466833a482785a9f64bb
Oka Widiawan <oka.widiawan@gmail.com>
2026-02-23 16:37:50 +0700

    Mencoba gunakan Math

---
+++ 8.math.js
@@ -0,0 +1,17 @@
+console.log(Math.max(10, 11, 9, 13, 8));
+// 13
+
+console.log(Math.min(10, 11, 9, 13, 8));
+// 8
+
+console.log(Math.round(10.5));
+// 11
+
+console.log(Math.round(10.3));
+// 10
+
+console.log(Math.floor(10.8));
+// 10
+
+console.log(Math.ceil(10.3));
+// 11
